2018523ead1ca7a8da287efa347644d9c4d9f8fc
petitoff <playgame12391@gmail.com>
2021-06-12 15:16:40 +0200

    Initial commit

---
+++ script.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+# from time import sleep
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class ScriptSelenium:
+    def __init__(self):
+        self.driver = webdriver.Chrome("C:\\Selenium\\chromedriver.exe")
+
+    def script(self):
+        # open selenium / webdriver
+        self.driver.get("https://instagram.com/")
+
+        # Accept cookies
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div/button[1]"))).click()
+
+        # login form
+        login = "jakubowski.elise5304@yousmail.com"
+        password = "doan913nf0ne20fns0"
+        button_path = "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button/div"
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, "username"))).send_keys(
+            login)  # login
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, "password"))).send_keys(
+            password)  # password
+        self.driver.find_element_by_xpath(button_path).click()
+
+        # accept save login
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "/html/body/div[1]/section/main/div/div/div/section/div/button"))).click()
+        # off notification
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/div/div[3]/button[2]"))).click()
+
+        print("Login Success")
---
+++ test.py
@@ -0,0 +1,4 @@
+from script import *
+
+ScriptSelenium().script()
+
