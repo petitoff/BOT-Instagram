9da9f223f578c9df8965fe2ef369c46aa4c7222d
petitoff <playgame12391@gmail.com>
2021-06-17 21:46:03 +0200

    wysyłanie wiadomości z pliku

--- script.py
+++ script.py
@@ -297,6 +297,7 @@ class ScriptSelenium:
         return "done"
 
     def instagram_main_user(self):
+        print("\nPut random messages in spam-message.txt\n")
         try:
             # select the first photo from the tab
             WebDriverWait(self.driver, 4).until(EC.element_to_be_clickable((By.CLASS_NAME, "_9AhH0")))
@@ -349,10 +350,30 @@ class ScriptSelenium:
         sleep(1)
         self.driver.find_element_by_class_name("sqdOP").click()  # click send msg
 
-        text_area_path = "/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea"
+        self.instagram_main_user_open_spam_message()
+
+        text_area_path = \
+            "/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea"
+        button_send_msg_path = \
+            "/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button"
+
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, text_area_path)))
         self.driver.find_element_by_xpath(text_area_path).click()
-        self.driver.find_element_by_xpath(text_area_path).send_keys("test")
+
+        lines_len = len(self.list_spam_comment)
+        random_index = randint(0, lines_len - 1)
+        random_comment = self.lines[random_index]
+        sleep(1)
+
+        self.driver.find_element_by_xpath(text_area_path).send_keys(random_comment)
+        self.driver.find_element_by_xpath(button_send_msg_path).click()
+
+    def instagram_main_user_open_spam_message(self):
+        self.list_spam_comment = []
+        with open("spam-message.txt") as file:
+            for line in file:
+                line = line.strip()
+                self.list_spam_comment.append(line)
 
     def instagram_user_explore_tab(self):
         self.driver.get("https://www.instagram.com/explore/")  # link pointing to the explore tab
