8679bc480471835b90c78152bb3bccf2a93e8cf1
petitoff <playgame12391@gmail.com>
2021-06-17 21:27:37 +0200

    wejście na profil i wysłanie wiadomości

--- script.py
+++ script.py
@@ -308,6 +308,7 @@ class ScriptSelenium:
         except selenium.common.exceptions.TimeoutException:
             return "error"
 
+        # kliknięcie w pierwszy komentarz
         error = 0
         while True:
             try:
@@ -347,6 +348,11 @@ class ScriptSelenium:
         self.driver.find_element_by_class_name("_5f5mN").click()  # click follow
         sleep(1)
         self.driver.find_element_by_class_name("sqdOP").click()  # click send msg
+
+        text_area_path = "/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea"
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, text_area_path)))
+        self.driver.find_element_by_xpath(text_area_path).click()
+        self.driver.find_element_by_xpath(text_area_path).send_keys("test")
 
     def instagram_user_explore_tab(self):
         self.driver.get("https://www.instagram.com/explore/")  # link pointing to the explore tab
